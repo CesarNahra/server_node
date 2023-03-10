32415986986a937e6e5af1b045882d6111833ff1
CesarNahra <cesarsantanna.nahra@gmail.com>
2023-03-10 18:20:08 -0300

    rota com envio de nome criado

--- iniciando_server_node/inicializando_server/index.js
+++ iniciando_server_node/inicializando_server/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/names/:name", (req, res) => {
+    const name = req.params.name;
+    console.log(`Rota de API criada pelo(a): ${name}`);
+    res.send(`Seu nome é: ${name}`);
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
